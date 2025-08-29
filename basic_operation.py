464aee4ef7f804d5f108380d374b2cbf9d2faa25
cslovelyjaiswal15-gif <cslovelyjaiswal15@gmail.com>
2025-08-29 11:46:13 +0530

    Add files via upload

---
+++ basic_operation.py
@@ -0,0 +1,19 @@
+
+# Q1 basic airthematic operation 
+a = int(input("Enter a  first number: "))
+
+b = int(input("Enter a second number: "))
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
+
+
+#Q2 formal greeting by input function
+# formal greeting
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+
+print("Hello, " + fname + " " + lname + "! Welcome! to tutedude platform.")
